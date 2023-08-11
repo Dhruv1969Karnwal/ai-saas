1445a924045be2dcf568633eca351da31bc3d47a
Dhruv Karnwal <kdhruv.fsd@gmail.com>
2023-08-11 22:57:27 +0530

    project-setUp-shadcn/ui@22:00

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <>
+    <p className='text-6xl text-red-500'>Hello Ai-Saas </p>
+    <Button variant='destructive'>Click me</Button>
+    </>
+  )
+}
