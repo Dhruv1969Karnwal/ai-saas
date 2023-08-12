4de5682a68ee308a998bb57432d8087d29489de0
Dhruv Karnwal <kdhruv.fsd@gmail.com>
2023-08-13 00:44:43 +0530

    register-page-added@12:30

--- app/(auth)/(routes)/register/page.tsx
+++ app/(auth)/(routes)/register/page.tsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import Link from "next/link"
 
-const page = () => {
+import { cn } from "@/lib/utils"
+import { buttonVariants } from "@/components/ui/button"
+import { UserAuthForm } from "@/components/user-auth-form"
+import { Icons } from "@/components/icons"
+import React from "react"
+
+export const metadata = {
+  title: "Create an account",
+  description: "Create an account to get started.",
+}
+
+const RegisterPage: React.FC = () => {
   return (
-    <div>page</div>
+    <div className="container grid h-screen w-screen flex-col items-center justify-center lg:max-w-none lg:grid-cols-2 lg:px-0">
+
+      <Link
+        href="/login"
+        className={cn(
+          buttonVariants({ variant: "ghost" }),
+          "absolute right-4 top-4 md:right-8 md:top-8"
+        )}
+      >
+        Login
+      </Link>
+      <div className="hidden h-full bg-muted lg:block" />
+      <div className="lg:p-8">
+        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px] ">
+          <div className="flex flex-col space-y-2 text-center">
+            <Icons.logo className="mx-auto h-6 w-6" />
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Create an account
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Enter your email below to create your account
+            </p>
+          </div>
+          <UserAuthForm />
+          <p className="px-8 text-center text-sm text-muted-foreground">
+            By clicking continue, you agree to our{" "}
+            <Link
+              href="/terms"
+              className="hover:text-brand underline underline-offset-4"
+            >
+              Terms of Service
+            </Link>{" "}
+            and{" "}
+            <Link
+              href="/privacy"
+              className="hover:text-brand underline underline-offset-4"
+            >
+              Privacy Policy
+            </Link>
+            .
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default page
+export default RegisterPage
