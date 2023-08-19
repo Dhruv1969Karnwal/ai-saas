84d627de401272e359fec89ecf9db92d204e146e
Dhruv Karnwal <kdhruv.fsd@gmail.com>
2023-08-19 23:54:45 +0530

    settings-page-added@23:45

---
+++ app/(dashboard)/(routes)/settings/page.tsx
@@ -0,0 +1,22 @@
+import { Settings } from "lucide-react";
+
+import { Heading } from "@/components/heading";
+
+const SettingsPage = async () => {
+
+  return ( 
+    <div>
+      <Heading
+        title="Settings"
+        description="Manage account settings."
+        icon={Settings}
+        iconColor="text-gray-700"
+        bgColor="bg-gray-700/10"
+      />
+      <div className="px-4 lg:px-8 space-y-4">
+      </div>
+    </div>
+   );
+}
+ 
+export default SettingsPage;
