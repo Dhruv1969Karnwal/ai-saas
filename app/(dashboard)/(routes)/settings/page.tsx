7f2bbc20296ba53a367121d2635033e7af9d5c58
Dhruv Karnwal <kdhruv.fsd@gmail.com>
2023-08-21 23:57:35 +0530

    ui-updated@23:00

--- app/(dashboard)/(routes)/settings/page.tsx
+++ app/(dashboard)/(routes)/settings/page.tsx
@@ -11,7 +11,7 @@ const SettingsPage = async () => {
         description="Manage account settings."
         icon={Settings}
         iconColor="text-gray-700"
-        bgColor="bg-gray-700/10"
+        bgColor="bg-gray-700/20"
       />
       <div className="px-4  lg:px-8 space-y-4">
       </div>
