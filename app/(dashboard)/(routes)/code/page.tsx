56a6aff5595a6abcdc042cd1f8da9a67a1cf5a38
Dhruv Karnwal <kdhruv.fsd@gmail.com>
2023-08-24 01:11:01 +0530

    react-markdown-codePage-added@01:30

--- app/(dashboard)/(routes)/code/page.tsx
+++ app/(dashboard)/(routes)/code/page.tsx
@@ -6,6 +6,7 @@ import { Code } from "lucide-react";
 import { useForm } from "react-hook-form";
 import { useRouter } from "next/navigation";
 import { ChatCompletionRequestMessage } from "openai";
+import ReactMarkdown from "react-markdown";
 
 import { Heading } from "@/components/heading";
 import { Button } from "@/components/ui/button";
@@ -102,19 +103,28 @@ const CodePage = () => {
           </Form>
         </div>
         <div className="flex flex-col-reverse gap-y-4">
-          {messages.map((message) => (
-            <div
-              key={message.content}
-              className={cn(
-                "p-8 w-full flex items-start gap-x-8 rounded-lg",
-                message.role === "user"
-                  ? "bg-white border border-black/10"
-                  : "bg-muted"
-              )}
-            >
-              <p className="text-sm">{message.content}</p>
-            </div>
-          ))}
+        {messages.map((message) => (
+              <div 
+                key={message.content} 
+                className={cn(
+                  "p-8 w-full flex items-start gap-x-8 rounded-lg",
+                  message.role === "user" ? "bg-white border border-black/10" : "bg-muted",
+                )}
+              >
+                <ReactMarkdown components={{
+                  pre: ({ node, ...props }) => (
+                    <div className="overflow-auto w-full my-2 bg-black/10 p-2 rounded-lg">
+                      <pre {...props} />
+                    </div>
+                  ),
+                  code: ({ node, ...props }) => (
+                    <code className="bg-black/10 rounded-lg p-1" {...props} />
+                  )
+                }} className="text-sm overflow-hidden leading-7">
+                  {message.content || ""}
+                </ReactMarkdown>
+              </div>
+            ))}
         </div>
       </div>
     </div>
