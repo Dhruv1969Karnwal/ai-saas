11efea7962071763ffc0c8ef1a3a9075563082af
Dhruv Karnwal <kdhruv.fsd@gmail.com>
2023-08-25 21:42:22 +0530

    companion-list-added@17:00

--- app/(dashboard)/(routes)/(conversation)/companion-ai/page.tsx
+++ app/(dashboard)/(routes)/(conversation)/companion-ai/page.tsx
@@ -1,16 +1,36 @@
-import prisma from "@/lib/db"
-import { SearchInput } from "@/components/search-input"
-import { Categories } from "@/components/categories"
+import prisma from "@/lib/db";
+import { SearchInput } from "@/components/search-input";
+import { Categories } from "@/components/categories";
+import { Companions } from "@/components/companions";
 interface RootPageProps {
   searchParams: {
     categoryId: string;
     name: string;
   };
-};
+}
 
-const RootPage = async ({
-  searchParams
-}: RootPageProps) => {
+const RootPage = async ({ searchParams }: RootPageProps) => {
+
+  const data = await prisma.companion.findMany({
+    where: {
+      categoryId: searchParams.categoryId,
+      name: {
+        contains: searchParams.name,
+      },
+    },
+    orderBy: {
+      createdAt: "desc"
+    },
+    include: {
+      _count: {
+        select: {
+          messages: true,
+        }
+      }
+    },
+  });
+
+
 
   const categories = await prisma.category.findMany();
 
@@ -18,8 +38,9 @@ const RootPage = async ({
     <div className="h-full p-4 space-y-2">
       <SearchInput />
       <Categories data={categories} />
+      <Companions data={data} />
     </div>
-  )
-}
+  );
+};
 
-export default RootPage
+export default RootPage;
