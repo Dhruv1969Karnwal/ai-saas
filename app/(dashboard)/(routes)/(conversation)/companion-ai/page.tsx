83634bd075849900a233fe5014d137aa539fbacf
Dhruv Karnwal <kdhruv.fsd@gmail.com>
2023-08-25 08:19:55 +0530

    category-component-(backend)-added@03:00

--- app/(dashboard)/(routes)/(conversation)/companion-ai/page.tsx
+++ app/(dashboard)/(routes)/(conversation)/companion-ai/page.tsx
@@ -1,4 +1,6 @@
+import prisma from "@/lib/db"
 import { SearchInput } from "@/components/search-input"
+import { Categories } from "@/components/categories"
 interface RootPageProps {
   searchParams: {
     categoryId: string;
@@ -10,9 +12,12 @@ const RootPage = async ({
   searchParams
 }: RootPageProps) => {
 
+  const categories = await prisma.category.findMany();
+
   return (
     <div className="h-full p-4 space-y-2">
       <SearchInput />
+      <Categories data={categories} />
     </div>
   )
 }
