827e0db7f0e8c47dc06a070f75ad8307c53d98da
Dhruv Karnwal <kdhruv.fsd@gmail.com>
2023-08-26 22:53:38 +0530

    api-limit-added-code-route@22:00

--- app/api/code/route.ts
+++ app/api/code/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import { ChatCompletionRequestMessage, Configuration, OpenAIApi } from "openai";
 import  getCurrentUser  from "@/lib/session"
+import { incrementApiLimit, checkApiLimit } from "@/lib/api-limit";
 
 
 const configuration = new Configuration({
@@ -35,11 +36,19 @@ export async function POST(
       return new NextResponse("Messages are required", { status: 400 });
     }
 
+    const freeTrial = await checkApiLimit();
+
+    if (!freeTrial) {
+      return new NextResponse("Free trial has expired. Please upgrade to pro.", { status: 403 });
+    }
+
     const response = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
       messages: [instructionMessage, ...messages]
     });
 
+    await incrementApiLimit();
+    
     return NextResponse.json(response.data.choices[0].message);
   } catch (error) {
     console.log('[CODE_ERROR]', error);
