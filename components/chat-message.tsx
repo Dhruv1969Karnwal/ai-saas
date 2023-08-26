28028c2287920bb282bc9167579c08de8f203048
Dhruv Karnwal <kdhruv.fsd@gmail.com>
2023-08-26 16:40:27 +0530

    loading-chat-messages-added@16:45

--- components/chat-message.tsx
+++ components/chat-message.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { BeatLoader } from "react-spinners";
+
+
 import { Copy } from "lucide-react";
 
 import { cn } from "@/lib/utils";
@@ -23,7 +26,6 @@ export const ChatMessage = ({
   src,
 }: ChatMessageProps) => {
 
-    // const currentUser = await getCurrentUser()
     const {data: session} = useSession()
   return (
     <div
@@ -34,7 +36,7 @@ export const ChatMessage = ({
     >
       {role !== "user" && src && <ChatBotAvatar src={src} />}
       <div className="rounded-md px-4 py-2 max-w-sm text-sm bg-primary/10">
-        {isLoading ? "Loading ..." : content}
+        {isLoading ? <BeatLoader  size={5} /> : content}
       </div>
       {role === "user" && (
         <UserAvatar
--- components/chat-messages.tsx
+++ components/chat-messages.tsx
@@ -8,7 +8,7 @@ import { ChatMessage } from "@/components/chat-message";
 interface ChatMessagesProps {
   messages: any[];
   isLoading: boolean;
-  companion: Companion;
+  companion: Companion
 }
 
 export const ChatMessages = ({
@@ -16,18 +16,34 @@ export const ChatMessages = ({
   isLoading,
   companion,
 }: ChatMessagesProps) => {
+
+  const [fakeLoading, setFakeLoading] = useState(messages.length === 0 ? true : false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setFakeLoading(false);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timeout);
+    }
+  }, []);
+
+
   return (
     <div className="flex-1 overflow-y-auto pr-4">
       <ChatMessage
+        isLoading={fakeLoading}
         src={companion.src}
         role="system"
         content={`Hello, I am ${companion.name}, ${companion.description}`}
       />
       <ChatMessage
-        // src={companion.src}
+        isLoading={fakeLoading}
         role="user"
         content={`Hello, I am ${companion.name}, ${companion.description}`}
       />
+
     </div>
   );
 };
