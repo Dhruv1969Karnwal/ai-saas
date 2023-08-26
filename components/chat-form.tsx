ad1f69bd0c15a32a92fbd48212a0bcdb1d989fcc
Dhruv Karnwal <kdhruv.fsd@gmail.com>
2023-08-26 16:07:39 +0530

    chat-form-ui-added@12:00

--- components/chat-form.tsx
+++ components/chat-form.tsx
@@ -1,9 +1,11 @@
 "use client";
 
 import { ChatRequestOptions } from "ai";
+import { Send } from "lucide-react";
 import { ChangeEvent, FormEvent } from "react";
 
-
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 interface ChatFormProps {
   input: string;
@@ -19,6 +21,17 @@ export const ChatForm = ({
   isLoading,
 }: ChatFormProps) => {
   return (
-    <div>ChatForm</div>
+    <form onSubmit={onSubmit} className="border-t border-primary/10 py-4 flex items-center gap-x-2">
+      <Input
+        disabled={isLoading}
+        value={input}
+        onChange={handleInputChange}
+        placeholder="Type a message"
+        className="rounded-lg bg-primary/10"
+      />
+      <Button disabled={isLoading} variant="ghost">
+        <Send className="w-6 h-6" />
+      </Button>
+    </form>
   )
 }
