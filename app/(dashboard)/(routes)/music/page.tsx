851d11ba6abbfd6308e28194330e68b0395712fa
Dhruv Karnwal <kdhruv.fsd@gmail.com>
2023-08-26 20:39:16 +0530

    music-api-functionality-added@20:30

--- app/(dashboard)/(routes)/music/page.tsx
+++ app/(dashboard)/(routes)/music/page.tsx
@@ -1,18 +1,26 @@
 "use client";
 
 import * as z from "zod";
+import axios from "axios";
+import { useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { Music } from "lucide-react";
+import { toast } from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import { Music, Send } from "lucide-react";
 
 import { Heading } from "@/components/heading";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { Loader } from "@/components/loader";
+import { Empty } from "@/components/ui/empty";
 
 import { formSchema } from "./constants";
 
 const MusicPage = () => {
+  const router = useRouter();
+  const [music, setMusic] = useState<string>();
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -24,6 +32,22 @@ const MusicPage = () => {
   const isLoading = form.formState.isSubmitting;
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      setMusic(undefined);
+
+      const response = await axios.post('/api/music', values);
+      console.log(response)
+
+      setMusic(response.data.audio);
+      form.reset();
+    } catch (error: any) {
+      if (error?.response?.status === 403) {
+      } else {
+        toast.error("Something went wrong.");
+      }
+    } finally {
+      router.refresh();
+    }
   }
 
   return ( 
@@ -72,6 +96,19 @@ const MusicPage = () => {
             </Button>
           </form>
         </Form>
+        {isLoading && (
+          <div className="p-20">
+            <Loader />
+          </div>
+        )}
+        {!music && !isLoading && (
+          <Empty label="No music generated." />
+        )}
+        {music && (
+          <audio controls className="w-full mt-8">
+            <source src={music} />
+          </audio>
+        )}
       </div>
     </div>
    );
