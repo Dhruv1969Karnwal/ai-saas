686edf7ab2382f3234c15589b3f73d6b0004e809
Dhruv Karnwal <kdhruv.fsd@gmail.com>
2023-08-26 22:44:56 +0530

    incrementApiLimit-functionality-added@22:45

---
+++ lib/api-limit.ts
@@ -0,0 +1,29 @@
+import prisma from "@/lib/db";
+import { MAX_FREE_COUNTS } from "@/constants";
+import getCurrentUser from "./session";
+
+export const incrementApiLimit = async () => {
+  const currentUser = await getCurrentUser();
+  const userId = currentUser?.userId;
+
+  if (!userId) {
+    return;
+  }
+
+  const userApiLimit = await prisma.userApiLimit.findUnique({
+    where: { userId: userId },
+  });
+
+  if (userApiLimit) {
+    await prisma.userApiLimit.update({
+      where: { userId: userId },
+      data: { count: userApiLimit.count + 1 },
+    });
+  } else {
+    await prisma.userApiLimit.create({
+      data: { userId: userId, count: 1 },
+    });
+  }
+};
+
+
