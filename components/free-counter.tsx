27942245cc8ec08ec604221f622d088fa99f5e9d
Dhruv Karnwal <kdhruv.fsd@gmail.com>
2023-08-27 11:25:26 +0530

    free-counter-ui-update@08:45

--- components/free-counter.tsx
+++ components/free-counter.tsx
@@ -1,16 +1,42 @@
+import { useEffect, useState } from "react";
 
+import { MAX_FREE_COUNTS } from "@/constants";
+import { Card, CardContent } from "@/components/ui/card";
+import { Button } from "./ui/button";
+import { Zap } from "lucide-react";
 
 export const FreeCounter = ({
   apiLimitCount = 0,
 }: {
   apiLimitCount: number
 }) => {
+  const [mounted, setMounted] = useState(false);
 
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+  
 
 
   return (
     <div className="px-3">
-        free counter
+      <Card className="bg-white/10 border-0">
+        <CardContent className="py-6">
+          <div className="text-center text-sm text-white mb-4 space-y-2">
+            <p>
+              {apiLimitCount} / {MAX_FREE_COUNTS} Free Generations
+            </p>
+          </div>
+          <Button  variant="premium" className="w-full">
+            Upgrade
+            <Zap className="w-4 h-4 ml-2 fill-white" />
+          </Button>
+        </CardContent>
+      </Card>
     </div>
   )
 }
