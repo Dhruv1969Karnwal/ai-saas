6245a81ea6d480875d14e81649973760b62ae7ea
Dhruv Karnwal <kdhruv.fsd@gmail.com>
2023-08-27 21:25:20 +0530

    free-counter-ui-update-mobile@20:30

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,7 +2,8 @@ import { MobileSidebar } from "@/components/mobile-sidebar";
 
 import { SafeUser } from "@/lib/safeUser";
 import { NavbarTop } from "@/components/navbar-top";
-import { getApiLimitCount } from "@/lib/api-limit";
+import {  getApiLimitCount } from "@/lib/api-limit";
+import { checkSubscription } from "@/lib/subscription";
 
 interface NavbarProps {
   currentUser?: SafeUser | null;
@@ -10,9 +11,10 @@ interface NavbarProps {
 
 export async  function Navbar({ currentUser }: NavbarProps) {
   const apiLimitCount = await getApiLimitCount();
+  const isPro = await checkSubscription()
   return (
     <div className="flex items-center p-4 justify-between">
-      <MobileSidebar apiLimitCount={apiLimitCount} />
+      <MobileSidebar apiLimitCount={apiLimitCount} isPro={isPro}/>
       <NavbarTop currentUser={currentUser} />
     </div>
   );
