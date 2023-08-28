75d11389416cdeaa5a3a0180dffdd0345303df82
Dhruv Karnwal <kdhruv.fsd@gmail.com>
2023-08-28 18:32:25 +0530

    mainchatbotheader-added@14:30

--- components/MainChatBot.tsx
+++ components/MainChatBot.tsx
@@ -6,6 +6,7 @@ import {
   AccordionItem,
   AccordionTrigger
 } from './ui/accordion'
+import MainChatBotHeader from './MainChatBotHeader'
 
 
 const MainChatBot: FC = () => {
@@ -13,12 +14,13 @@ const MainChatBot: FC = () => {
     <Accordion
       type='single'
       collapsible
-      className='relative bg-white z-40 shadow'>
-      <AccordionItem value='item-1'>
+      className='relative bg-white z-40 shadow '>
+      <AccordionItem value='item-1' className=''>
         <div className='fixed right-8 w-80 bottom-8 bg-white border border-gray-200 rounded-md overflow-hidden'>
           <div className='w-full h-full flex flex-col'>
-          <AccordionTrigger className='px-6 border-b border-zinc-300'>
-          </AccordionTrigger>
+            <AccordionTrigger className='px-6 border-b border-zinc-300 py-4'>
+              <MainChatBotHeader />
+            </AccordionTrigger>
           </div>
         </div>
       </AccordionItem>
