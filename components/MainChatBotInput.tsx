93a61fbc11c49da9b58f792587c3e6a39bbca31b
Dhruv Karnwal <kdhruv.fsd@gmail.com>
2023-08-28 19:30:51 +0530

    chat-message-send-to-backend@16:00

--- components/MainChatBotInput.tsx
+++ components/MainChatBotInput.tsx
@@ -1,31 +1,62 @@
-'use client'
+"use client";
 
-import { cn } from '@/lib/utils'
-import { FC, HTMLAttributes, useState} from 'react'
-import TextareaAutosize from 'react-textarea-autosize'
+import { cn } from "@/lib/utils";
+import { useMutation } from "@tanstack/react-query";
+import { FC, HTMLAttributes, useState } from "react";
+import TextareaAutosize from "react-textarea-autosize";
+import { nanoid } from 'nanoid'
+import { Message } from "@/lib/validations/message";
 
 interface ChatInputProps extends HTMLAttributes<HTMLDivElement> {}
 
 const MainChatBotInput: FC<ChatInputProps> = ({ className, ...props }) => {
+  const [input, setInput] = useState<string>("");
 
-  const [input, setInput] = useState<string>('')
+  const { mutate: sendMessage, isLoading } = useMutation({
+    mutationFn: async (_message: Message) => {
+      const response = await fetch("/api/message", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ messages: "hello" }),
+      });
 
+      return response.body;
+    },
+    onSuccess: () => {
+      console.log("success")
+    }
+  });
 
   return (
-    <div {...props} className={cn('border-t border-zinc-300', className)}>
-      <div className='relative mt-4 flex-1 overflow-hidden rounded-lg border-none outline-none'>
+    <div {...props} className={cn("border-t border-zinc-300", className)}>
+      <div className="relative mt-4 flex-1 overflow-hidden rounded-lg border-none outline-none">
         <TextareaAutosize
+        onKeyDown={(e) => {
+          if (e.key === 'Enter' && !e.shiftKey) {
+            e.preventDefault()
+
+            const message: Message = {
+              id: nanoid(),
+              isUserMessage: true,
+              text: input,
+            }
+
+            sendMessage(message)
+          }
+        }}
           rows={2}
           value={input}
           onChange={(e) => setInput(e.target.value)}
           maxRows={4}
           autoFocus
-          placeholder='Write a message...'
-          className='peer disabled:opacity-50 pr-14 resize-none block w-full border-0 bg-zinc-100 py-1.5 text-gray-900 focus:ring-0 text-sm sm:leading-6 border-x-0 border-y-0'
+          placeholder="Write a message..."
+          className="peer disabled:opacity-50 pr-14 resize-none block w-full border-0 bg-zinc-100 py-1.5 text-gray-900 focus:ring-0 text-sm sm:leading-6 border-x-0 border-y-0"
         />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default MainChatBotInput
+export default MainChatBotInput;
