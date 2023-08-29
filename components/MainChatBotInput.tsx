f4bb1aba12402d21808d32df9f32f263e1645b02
Dhruv Karnwal <kdhruv.fsd@gmail.com>
2023-08-29 12:50:50 +0530

    mainchatbotmessage-ui-update@10:00

--- components/MainChatBotInput.tsx
+++ components/MainChatBotInput.tsx
@@ -43,7 +43,7 @@ const MainChatBotInput: FC<ChatInputProps> = ({ className, ...props }) => {
 
   return (
     <div {...props} className={cn("border-t border-zinc-300", className)}>
-      <div className="relative mt-4 flex-1 overflow-hidden rounded-lg border-none outline-none">
+      <div className="relative mt-4 mb-4 flex-1 overflow-hidden rounded-lg border-none outline-none">
         <TextareaAutosize
         onKeyDown={(e) => {
           if (e.key === 'Enter' && !e.shiftKey) {
@@ -64,7 +64,7 @@ const MainChatBotInput: FC<ChatInputProps> = ({ className, ...props }) => {
           maxRows={4}
           autoFocus
           placeholder="Write a message..."
-          className="peer disabled:opacity-50 pr-14 resize-none block w-full border-0 bg-zinc-100 py-1.5 text-gray-900 focus:ring-0 text-sm sm:leading-6 border-x-0 border-y-0"
+          className="peer disabled:opacity-50 pr-4 pl-4 resize-none block w-full border-0 bg-zinc-100 py-1.5 text-gray-900 focus:ring-0 text-sm sm:leading-6 border-x-0 border-y-0"
         />
       </div>
     </div>
